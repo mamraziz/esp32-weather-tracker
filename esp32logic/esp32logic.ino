36e65eae27f6d3735f0ad1b771327c5f4dae8dc1
mamraziz <mamrmaziz@gmail.com>
2026-09-02 19:06:56 +0300

    added initial sensor code

--- esp32logic/esp32logic.ino
+++ esp32logic/esp32logic.ino
@@ -6,6 +6,13 @@
 //const char* password = "";
 //remove screts.h and uncomment ssid and password
 
+int ldrS = 3;
+int tempS = 5;
+int humS = 6;
+int ldr;
+int temp;
+int hum;
+
 HTTPClient http;
 
 void setup() {
@@ -14,14 +21,23 @@ void setup() {
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
   }
+  pinMode(ldrS, INPUT);
+  pinMode(tempS, INPUT);
+  pinMode(humS, INPUT);
+  Serial.begin(9600); // delete this
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  ldr = analogRead(ldrS);
+  temp = analogRead(tempS); 
+  hum = analogRead(humS);
+  Serial.println(ldr + temp + hum); // delete this
+
   http.begin("http://192.168.1.11:3000/readings");
   http.addHeader("Content-Type", "application/json");
-  String body = "{\"ldr\": 42}";
+  String body = "{\"ldr\": " + String(ldr) + ", \"temp\": " + String(temp) + ", \"hum\": " + String(hum) + "}";
   int responseCode = http.POST(body);
   http.end();
-  delay(3000);
+  delay(30000);
 }
